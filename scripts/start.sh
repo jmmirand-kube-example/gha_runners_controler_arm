1e97ffc4d995f21aab5ddf5fc53f49643745952f
Jose Miguel <jmmirand@gmail.com>
2021-01-31 20:25:54 +0100

    fix start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-ORGANIZATION=$ORGANIZATION
-ACCESS_TOKEN=$ACCESS_TOKEN
-NAME=$RUNNER_NAME
-
 REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner
 
 ./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN} \
-  --labels ${LABELS} --name $RUNNER_NAME \
+  --labels ${RUNNER_LABELS} \
+  --name $RUNNER_NAME \
   --replace
 
 cleanup() {
