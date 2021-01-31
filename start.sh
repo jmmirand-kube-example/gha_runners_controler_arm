c28d40ae60f6891bc564f501f04bdcb9a65098c4
Jose Miguel <jmmirand@gmail.com>
2021-01-31 19:15:15 +0100

    delete name

--- start.sh
+++ start.sh
@@ -3,11 +3,12 @@
 ORGANIZATION=$ORGANIZATION
 ACCESS_TOKEN=$ACCESS_TOKEN
 
+
 REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner
 
-./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}
+./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN} --label ${RUNNER_LABELS}
 
 cleanup() {
     echo "Removing runner..."
